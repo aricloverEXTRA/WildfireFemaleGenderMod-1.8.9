6196fd02d55dd136a49047d6441dbb7365bad317
aricloverEXTRA <157071384+aricloverEXTRA@users.noreply.github.com>
2026-09-08 11:41:47 -0500

    UP UVs improvements
    also fixes scaling issues with the UP UVs

--- src/main/java/com/wildfire/main/uvs/UVLayout.java
+++ src/main/java/com/wildfire/main/uvs/UVLayout.java
@@ -68,8 +68,8 @@ public class UVLayout {
         UVLayout layout = new UVLayout();
         layout.put(UVDirection.EAST, new UVQuad(24, 21, 28, 26));
         layout.put(UVDirection.WEST, new UVQuad(16, 21, 20, 26));
-        layout.put(UVDirection.DOWN, new UVQuad(20, 21, 24, 17));
-        layout.put(UVDirection.UP, new UVQuad(24, 25, 20, 27));
+        layout.put(UVDirection.DOWN, new UVQuad(20, 17, 24, 21));
+        layout.put(UVDirection.UP, new UVQuad(20, 25, 24, 27));
         layout.put(UVDirection.NORTH, new UVQuad(20, 21, 24, 26));
         return layout;
     }
@@ -77,9 +77,9 @@ public class UVLayout {
     public static UVLayout defaultsForRight() {
         UVLayout layout = new UVLayout();
         layout.put(UVDirection.EAST, new UVQuad(28, 21, 32, 26));
-        layout.put(UVDirection.WEST, new UVQuad(20, 21, 24, 26));
-        layout.put(UVDirection.DOWN, new UVQuad(24, 21, 28, 17));
-        layout.put(UVDirection.UP, new UVQuad(28, 25, 24, 27));
+        layout.put(UVDirection.WEST, new UVQuad(21, 21, 24, 26));
+        layout.put(UVDirection.DOWN, new UVQuad(24, 17, 28, 21));
+        layout.put(UVDirection.UP, new UVQuad(24, 25, 28, 27));
         layout.put(UVDirection.NORTH, new UVQuad(24, 21, 28, 26));
         return layout;
     }
